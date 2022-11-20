a2f0bd72fd7ebbec663588b5609ef43733241990
Luca <74325375+lucasauer@users.noreply.github.com>
2022-11-20 21:38:46 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,150 @@
+library(shiny)
+library(ggplot2)
+library(mapdata)
+
+## TODO: Klein ohne Gross, und fertige Daten hier schon einlesen (also orte und
+## nur_klein)
+
+setwd("~/Desktop/Orte mit Wenden")
+load("dat.RData")
+load("nur_klein.RData")
+orte <- dat[, c("ORT_NAME", "ORT_LAT", "ORT_LON", "POSTLEITZAHL")]
+orte$ORT_NAME <- tolower(orte$ORT_NAME)
+
+namen <- sort(c("wenig", "wendisch", "wind", "böhmisch", "welsch", "klein", 
+                "winn"))
+
+
+## Einstellen der Farben
+
+## Standardfarbpalette ggplot
+gg_color_hue <- function(n) {
+    hues = seq(15, 375, length = n + 1)
+    hcl(h = hues, l = 65, c = 100)[1:n]
+}
+colors <- gg_color_hue(7)
+
+## Variable fuer die Farben
+set_color <- function(data, names) {
+    n <- length(names)
+    color <- character(nrow(data))
+    colors <- gg_color_hue(n)
+    for(name in names) {
+        pos <- grepl(name, orte$ORT_NAME, fixed = TRUE)
+        color[pos] <- ifelse(color[pos] == "", name, color[pos])
+    }
+    ifelse(color == "", NA, color)
+}
+
+orte$color <- set_color(orte, namen)
+
+## Karte von Deutschland
+germany <- map_data("worldHires", region = "Germany")
+map_de <- ggplot() + 
+    geom_polygon(data = germany, 
+                 aes(x = long, y = lat, group = group), 
+                 fill = "grey", alpha = 0.8) +
+    theme_bw() + 
+    theme(aspect.ratio = 1, axis.title.x=element_blank(),
+          axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+          axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+          axis.title.y = element_blank(), panel.border = element_blank(), 
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+          axis.line = element_line(colour = "white"))
+
+
+## Extrahiere zu plottende Orte
+extract_orte <- function(names = namen, os = orte) {
+    o <- data.frame()
+    for(n in seq_along(names)) {
+        o <- rbind(o, os[grepl(names[n], os$ORT_NAME, fixed = TRUE) & os$color == names[n], ]) 
+    }
+    o
+}
+
+## Plotte verschiedene Ortsnamen
+plotArbitraryNames <- function(names, kOG = FALSE, preset_colors = TRUE) {
+    if(length(names) < 1) {
+        map_de
+    } else {
+        if((!kOG) | (kOG & !("klein" %in% names))) {
+            orte <- extract_orte(names)
+        } else {
+            os <- orte[!grepl("klein", orte$ORT_NAME, fixed = TRUE), ]
+            os <- rbind(os, orte[orte$ORT_NAME %in% nur_klein, ])
+            orte <- extract_orte(names = names, os = os)
+        }
+        plt <- map_de +
+            geom_point(data = orte, aes(x = ORT_LON, y = ORT_LAT, col = color)) +
+            labs(col = "Orte mit... im Namen") +
+            theme(legend.title = element_text(size = 14),
+                  legend.text = element_text(size = 12))  
+        if(preset_colors & length(names) > 0) {
+            index <- sapply(names, function(n) which(namen == n))
+            plt + scale_color_manual(values = colors[index])
+        } else {
+            plt
+        }   
+    }
+}
+
+## Klein ohne Gross
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Verteilung bestimmter deutscher Ortsnamen"),
+    
+    mainPanel(
+        plotOutput("map")
+    ),
+    
+    # Sidebar with a slider input for number of bins 
+    fixedRow(
+        
+        column(3, 
+               checkboxGroupInput("checkGroup", 
+                                  h3("Auswahl darzustellender Ortsnamen"), 
+                                  choices = list("böhmisch" = 1, 
+                                                 "klein" = 2, 
+                                                 "welsch" = 3,
+                                                 "wendisch" = 4,
+                                                 "wenig" = 5,
+                                                 "wind" = 6,
+                                                 "winn" = 7),
+                                  selected = 5)),
+        
+        column(3,
+               h3("Klein ohne zugehöriges Groß"),
+               checkboxInput("klein", "Klein ohne Groß", value = FALSE)),
+        
+        
+        column(3,
+               h3("Bestätigen"),
+               submitButton("Submit"))
+        
+    ),
+    
+    # sidebarPanel(
+    #     textOutput("test")
+    # )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    output$map <- renderPlot({ 
+        plotArbitraryNames(names = namen[sort(as.numeric(input$checkGroup))],
+                           kOG = input$klein)
+
+    })
+    
+    # output$test <- renderText({
+    #     str(input$checkGroup)
+    # })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
